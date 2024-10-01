b808b337e29947cfae5fa39d710b635f561d01b4
Maxhild <Maxhild10@gmail.com>
2024-10-01 17:27:39 +0200

    fitz -> pymupdf

--- flake.nix
+++ flake.nix
@@ -3,26 +3,21 @@
 
   nixConfig = {
     substituters = [
-        "https://cache.nixos.org"
-        "https://cuda-maintainers.cachix.org"
-      ];
+      "https://cache.nixos.org"
+      "https://cuda-maintainers.cachix.org"
+    ];
     trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      ];
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
     extra-substituters = "https://cache.nixos.org https://nix-community.cachix.org https://cuda-maintainers.cachix.org";
     extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=";
   };
 
-
   inputs = {
-    # Use a single nixpkgs input to avoid conflicts
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # poetry2nix should follow the same nixpkgs
     poetry2nix.url = "github:nix-community/poetry2nix";
     poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
-
     cachix = {
       url = "github:cachix/cachix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -32,16 +27,15 @@
   outputs = { nixpkgs, cachix, ... } @ inputs:
   let
     nvidiaCache = cachix.lib.mkCachixCache {
-        inherit (pkgs) lib;
-        name = "nvidia";
-        publicKey = "nvidia.cachix.org-1:dSyZxI8geDCJrwgvBfPH3zHMC+PO6y/BT7O6zLBOv0w=";
-        secretKey = null;  # not needed for pulling from the cache
-      };
+      inherit (pkgs) lib;
+      name = "nvidia";
+      publicKey = "nvidia.cachix.org-1:dSyZxI8geDCJrwgvBfPH3zHMC+PO6y/BT7O6zLBOv0w=";
+      secretKey = null;
+    };
 
     system = "x86_64-linux";
     self = inputs.self;
 
-    # Import nixpkgs with desired configuration
     pkgs = import nixpkgs {
       inherit system;
       config = {
@@ -58,16 +52,21 @@
       wikipedia-api = [ "setuptools" ];
       django-flat-theme = [ "setuptools" ];
       django-flat-responsive = [ "setuptools" ];
-      # transformers = [ "maturin" ];
     };
 
-
-    poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs;};
+    poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
 
     lib = pkgs.lib;
 
+    # Define mupdf-shared
+    mupdf-shared = pkgs.mupdf.overrideAttrs (oldAttrs: rec {
+      enableSharedLibraries = true;
+      NIX_CFLAGS_COMPILE = oldAttrs.NIX_CFLAGS_COMPILE or "" + " -fPIC";
+    });
+
+    # Adjust p2n-overrides to include pymupdf override
     p2n-overrides = poetry2nix.defaultPoetryOverrides.extend (final: prev:
-      builtins.mapAttrs (package: build-requirements:
+      (builtins.mapAttrs (package: build-requirements:
         (builtins.getAttr package prev).overridePythonAttrs (old: {
           buildInputs = (old.buildInputs or [ ]) ++ (
             builtins.map (pkg:
@@ -75,44 +74,41 @@
             ) build-requirements
           );
         })
-      ) pypkgs-build-requirements
+      ) pypkgs-build-requirements)
+      // {
+        pymupdf = prev.pymupdf.overridePythonAttrs (oldAttrs: rec {
+          buildInputs = (oldAttrs.buildInputs or []) ++ [ mupdf-shared ];
+        });
+      }
     );
 
     poetryApp = poetry2nix.mkPoetryApplication {
       python = pkgs.python311;
       projectDir = ./.;
       src = lib.cleanSource ./.;
-      # groups = ["dev"];
       overrides = p2n-overrides;
-      
-      preferWheels = true; # required for transformers via p2n
-
+      preferWheels = true;
       propagatedBuildInputs =  with pkgs.python311Packages; [];
-
       nativeBuildInputs = with pkgs; [
         python311Packages.pip
         python311Packages.setuptools
         python311Packages.torch-bin
         python311Packages.torchvision-bin
         python311Packages.torchaudio-bin
-        python311Packages.pymupdf
+        # Removed python311Packages.pymupdf
       ];
     };
-    
+
   in {
-
     nixConfig = {
-        binary-caches = [
-          nvidiaCache.binaryCachePublicUrl
-        ];
-        binary-cache-public-keys = [
-          nvidiaCache.publicKey
-        ];
-        # enable cuda support
-        cudaSupport = true;
-      };
-
-    
+      binary-caches = [
+        nvidiaCache.binaryCachePublicUrl
+      ];
+      binary-cache-public-keys = [
+        nvidiaCache.publicKey
+      ];
+      cudaSupport = true;
+    };
 
     packages.x86_64-linux.poetryApp = poetryApp;
     packages.x86_64-linux.default = poetryApp;
@@ -121,7 +117,5 @@
       inputsFrom = [ self.packages.x86_64-linux.poetryApp ];
       packages = [ pkgs.poetry ];
     };
-
-
   };
 }
