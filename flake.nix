956c3d4f175e2770d59f6047f8e5e606888507af
Maxhild <Maxhild10@gmail.com>
2024-11-18 17:08:10 +0100

    export triton_ptxas_path - desperate attempts

--- flake.nix
+++ flake.nix
@@ -342,7 +342,7 @@
         apps.agl_anonymizer_pipeline = {
           buildPhase = ''
             maturin build --release -m pyproject.toml
-            export LLVM_SYS_150_PREFIX=${llvm_18}
+            export LLVM_SYS_150_PREFIX=${pkgs.llvm_18}
             export RUSTFLAGS="-C link-arg=-L${pkgs.cudatoolkit}/lib64 -C link-arg=-lcudart -C link-arg=-lcudnn"
             rustup target add x86_64-unknown-linux-gnu
           '';
