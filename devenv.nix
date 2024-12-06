e33ed5de44745fe0463f57d37f055cbd7f857133
maxhild <58726316+maxhild@users.noreply.github.com>
2024-12-06 17:22:17 +0100

    package name

--- devenv.nix
+++ devenv.nix
@@ -3,7 +3,6 @@
 let
   # Check if we're on a CUDA-compatible system (x86_64-linux)
   isCudaSupported = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
-  stdenv = pkgs.stdenv;
 
   # CUDA packages that will only be included on supported systems
   cudaPackages = if isCudaSupported then with pkgs; [
@@ -44,25 +43,18 @@ let
   };
 in
 {
-  # Basic language support
-  myDerivation = stdenv.mkDerivation {
-    pname = "agl_anonymizer_pipeline";
-    version = "0.1.12";
-    # other attributes
-  };
+  # Combine common packages with conditional CUDA packages
+  packages = commonPackages ++ cudaPackages;
 
+  # Basic language support
   languages.python = {
     enable = true;
     package = pkgs.python311;
     venv.enable = true;
   };
 
-  # Combine common packages with conditional CUDA packages
-  packages = commonPackages ++ cudaPackages;
-
   # Combined environment variables
   env = {
-
     NIX_PATH = "nixpkgs=${pkgs.path}";
     PYTHON_VERSION = "3.11.9";
     CUDA_ENABLED = if isCudaSupported then "1" else "0";
@@ -81,7 +73,6 @@ in
       echo "CUDA is not available on this platform"
     fi
   '';
-
 
   # Define processes
   processes.my_python_app = {
